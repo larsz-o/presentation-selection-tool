2fad1fd32473db7e0c66722f6fa91b377f9fad3c
Lars <larsmackenzie@gmail.com>
2019-08-13 13:51:38 -0400

    set up project structure and basic ui

--- src/App.js
+++ src/App.js
@@ -1,26 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class App extends Component {
+    constructor(props){
+      super(props);
+      this.state = {
+        signals: [{signal: '', student: '', claimed: false}],
+        open: false
+      }
+    }
+  
+    openDialogue = () => {
+      this.setState({
+        ...this.state, 
+        open: true
+      })
+    }
+  render() {
+    return(
+      <main>
+        <h1>Signalling Pathway Presentations</h1>
+        <h2>Claim the signalling transduction pathway you'd like to present on.</h2>
+        <ol type="1">
+          {this.state.signals.map((signal, i) => {
+            return(
+              <li key={i}>{signal.signal} {signal.claimed ? (<button onClick={()=>this.openDialgoue()}>Claim</button>) : (<p>Already claimed</p>)}</li>
+            )
+          })}
+        </ol>
+      </main>
+    );
+}}
 
 export default App;
