a01799c5a1ec4eacca6e09b95e76bd72fb6fbe27
Lars <larsmackenzie@gmail.com>
2019-08-26 12:24:08 -0400

    filtering topics

--- src/components/InstructorView/InstructorView.js
+++ src/components/InstructorView/InstructorView.js
@@ -3,7 +3,6 @@ import Header from '../Header/Header';
 import axios from 'axios';
 import { Dialog, DialogTitle, Select, MenuItem, Input } from '@material-ui/core';
 import swal from 'sweetalert'; 
-import TopicsTable from './TopicsTable';
 
 class InstructorView extends Component {
     constructor(props) {
@@ -12,6 +11,7 @@ class InstructorView extends Component {
             topics: [],
             categories: [],
             activeTerm: [],
+            editingTopic: '',
             newTopic: '',
             activeTopic: '',
             term: '',
@@ -19,9 +19,21 @@ class InstructorView extends Component {
             year: 0,
             category: '',
             termEdit: false,
+            open: false,
             newDialog: false,
             editCategory: true
         }
+    }
+    applyFilter = () => {
+        let topics = this.state.topics; 
+        if (this.state.filter !== ''){
+            topics.filter(topic => topic.category === this.state.filter); 
+        }
+        console.log(topics);
+        this.setState({
+            ...this.state,
+            topics: topics
+        })
     }
     closeDialogue = () => {
         this.setState({
@@ -33,6 +45,33 @@ class InstructorView extends Component {
     componentDidMount = () => {
         this.getLatestData();
         this.getActiveTerm();
+    }
+    deleteTopic = (topic) => {
+        if (window.confirm(`Are you sure you want to delete ${topic.topic}?`)) {
+            axios({
+                method: 'DELETE',
+                url: 'api/topics',
+                data: topic
+            }).then(() => {
+                this.getLatestData();
+                swal('Success', 'The topic has been deleted', 'success');
+            }).catch((error) => {
+                console.log('Error deleting', error);
+            })
+        }
+    }
+    editTopic = () => {
+        axios({
+            method: 'PUT',
+            url: 'api/topics',
+            data: this.state.editingTopic
+        }).then(() => {
+            this.getLatestData();
+            this.closeDialogue();
+            swal('Success', 'The topic has been edited', 'success');
+        }).catch((error) => {
+            console.log('Error updating', error);
+        })
     }
     getActiveTerm = () => {
         axios({
@@ -75,11 +114,24 @@ class InstructorView extends Component {
             console.log('Error getting topics', error);
         })
     }
+    handleChangeFor = (event) => {
+        this.setState({
+            ...this.state,
+            editingTopic: { ...this.state.editingTopic, topic: event.target.value }
+        })
+    }
     handleTermChange = (event, property) => {
         this.setState({
             ...this.state,
             [property]: event.target.value
         })
+    }
+    openDialogue = (topic) => {
+        this.setState({
+            ...this.state,
+            open: true,
+            editingTopic: topic
+        });
     }
     openNewDialogue = () => {
         this.setState({
@@ -127,10 +179,6 @@ class InstructorView extends Component {
         this.setState({ ...this.state, editCategory: false });
     }
     render() {
-        let topics = this.state.topics; 
-        if (this.state.filter !== ''){
-            topics.filter(topic => topic.category === this.state.filter); 
-        }
         return (
             <main>
                 <Header term={this.state.activeTerm} />
@@ -156,29 +204,62 @@ class InstructorView extends Component {
                     {!this.state.termEdit && <p onClick={() => this.setState({ ...this.state, termEdit: true })} className="cancel link">Edit term display dates</p>}</div>
                     {/* to do: filter results */}
                     <div className="flex-box col-11">
-                        <label>Filter by topic: </label><Select onChange={(event)=>this.handleTermChange(event, 'filter')} value={this.state.filter}>
-                        <MenuItem value="">---</MenuItem>
+                     <select onChange={(event)=>this.handleTermChange(event, 'filter')} value={this.state.filter}>
+                        <option value="">View All</option>
                         {this.state.categories.map((category, i) => {
                             return(
-                                <MenuItem value={category} key={i}>{category}</MenuItem>
+                                <option value={category} key={i}>{category}</option>
                             );
                         })}
-                        </Select>
+                        </select><button className="save-button" onClick={()=>this.applyFilter()}>Filter</button>
                     </div>
-                    <TopicsTable getLatestData={this.getLatestData} topics={topics} closeDialogue={this.closeDialogue}/>
+                <div className="container">
+                    <table>
+                        <thead>
+                            <tr>
+                                <td>Topic</td>
+                                <td>Category</td>
+                                <td>Actions</td>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {this.state.topics.map((topic, i) => {
+                                return (
+                                    <tr key={i}>
+                                        <td>{topic.topic}</td>
+                                        <td>{topic.category}</td>
+                                        <td><button onClick={() => this.openDialogue(topic)}>Edit</button><button onClick={() => this.deleteTopic(topic)}>Delete</button></td>
+                                    </tr>
+                                );
+                            })}
+                        </tbody>
+                    </table>
+                </div>
+                {/* edit an existing topic */}
+                <Dialog open={this.state.open}>
+                    <div className="dialog-form">
+                        <DialogTitle>Enter topic information</DialogTitle>
+                        <label>Topic: </label><Input value={this.state.editingTopic.topic} onChange={(event) => this.handleChangeFor(event)} />
+                        <div className="flex-box">
+                            <p className="cancel" onClick={() => this.closeDialogue()}>Cancel</p>
+                            <button onClick={() => this.editTopic()}>Submit</button>
+                        </div>
+
+                    </div>
+                </Dialog>
                 {/* create a new topic */}
                 <Dialog open={this.state.newDialog}>
                     <div className="dialog-form">
                         <DialogTitle>Enter topic information</DialogTitle>
                         <label>Topic: </label><Input value={this.state.newTopic} onChange={(event) => this.handleTermChange(event, 'newTopic')} />
-                        <label>Category:</label><Select value={this.state.category} onChange={(event)=>this.handleTermChange(event, 'category')}>
-                            <MenuItem value="">---</MenuItem>
+                        <label>Category:</label><select value={this.state.category} onChange={(event)=>this.handleTermChange(event, 'category')}>
+                            <option value="">---</option>
                             {this.state.categories.map((category, i) => {
                                 return (
-                                    <MenuItem key={i} value={category}>{category}</MenuItem>
+                                    <option key={i} value={category}>{category}</option>
                                 );
                             })}
-                        </Select>
+                        </select>
                         {this.state.editCategory ? (<p className="cancel link" onClick={() => this.toggleEdit()}>Category not listed?</p>): (<div><p>Create a new category: </p><Input onChange={(event) => this.handleTermChange(event, 'category')} /></div>) }
                         <div className="flex-box">
                             <p className="cancel" onClick={() => this.closeDialogue()}>Cancel</p>
